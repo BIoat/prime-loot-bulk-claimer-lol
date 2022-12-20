f241340e5edfb0f114a6b1506aa023ec6c7f25eb
sleep <49450642+BIoat@users.noreply.github.com>
2022-12-20 05:37:38 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use core::time;
+use image::{buffer, open, DynamicImage, ImageBuffer, RgbImage};
+use scraper::{Html, Selector};
+use std::{
+    io,
+    path::{self, Path},
+    time::Duration,
+};
+
+use show_image::{create_window, event, ImageInfo, ImageView};
 use thirtyfour::prelude::*;
+use tokio::time::{sleep, Sleep};
+
+async fn solve_captcha(buffer: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let image = ImageView::new(ImageInfo::rgba8(200, 70), buffer);
+
+    // Create a window with default options and display the image.
+    let window = create_window("image", Default::default())?;
+    window.set_image("image-001", image)?;
+    for event in window.event_channel()? {
+        if let event::WindowEvent::KeyboardInput(event) = event {
+            println!("{:#?}", event);
+            if event.input.key_code == Some(event::VirtualKeyCode::Escape)
+                && event.input.state.is_pressed()
+            {
+                break;
+            }
+        }
+    }
+    Ok(())
+}
 async fn claim(account: Acc) -> WebDriverResult<()> {
     let caps = DesiredCapabilities::firefox();
     let driver = WebDriver::new("http://localhost:4444", caps).await?;
 
-    // Navigate to https://wikipedia.org.
     driver.goto("https://gaming.amazon.com/loot/lol10").await?;
 
     let loginb = driver
@@ -17,10 +46,10 @@ async fn claim(account: Acc) -> WebDriverResult<()> {
     loginb.click().await?;
 
     let amazon_e = driver.query(By::Id("ap_email")).first().await?;
-    amazon_e.send_keys(account.username).await?;
+    amazon_e.send_keys(&account.username).await?;
 
     let amazon_p = driver.query(By::Id("ap_password")).first().await?;
-    amazon_p.send_keys(account.password).await?;
+    amazon_p.send_keys(&account.password).await?;
 
     driver
         .query(By::Id("signInSubmit"))
@@ -28,6 +57,38 @@ async fn claim(account: Acc) -> WebDriverResult<()> {
         .await?
         .click()
         .await?;
+    let amazon_e = driver.query(By::Id("ap_email")).first().await?;
+    amazon_e.send_keys(&account.username).await?;
+
+    let amazon_p = driver.query(By::Id("ap_password")).first().await?;
+    amazon_p.send_keys(account.password).await?;
+
+    let captcha_pic = driver
+        .query(By::Id("auth-captcha-image-container"))
+        .first()
+        .await?;
+
+    captcha_pic.wait_until().displayed().await?;
+    let innerhtml = captcha_pic.inner_html().await?;
+    println!("inner html: {}", &innerhtml);
+    let fragment = Html::parse_fragment(&innerhtml);
+    let selector = Selector::parse("img").unwrap();
+    for element in fragment.select(&selector) {
+        println!("inner html: {}", &element.value().name());
+    }
+
+    // sleep(Duration::from_millis(1200)).await;
+    // captcha_pic.screenshot(Path::new("./captcha.png")).await?;
+    // sleep(Duration::from_millis(2200)).await;
+    // captcha_pic.screenshot(Path::new("./captcha2.png")).await?;
+    // let rgba = open("./captcha.png").unwrap().into_rgba8();
+    // let solution = solve_captcha(&rgba).await;
+    // print!(
+    //     "{}",
+    //     match solution {
+    //         _ => false,
+    //     }
+    // );
 
     driver.quit().await?;
 
@@ -45,6 +106,7 @@ struct Acc {
 }
 
 #[tokio::main]
+#[show_image::main]
 async fn main() {
     let account = Acc {
         accounttype: AccountType::Amazon,
